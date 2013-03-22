31917ca08ef42fe05992157decd9d316cea9d167
Ruben Kostucki <rubenkostucki@gmail.com>
2013-03-22 16:58:48 +0000

    start of project with all original files

---
+++ lib/land.rb
@@ -0,0 +1,30 @@
+require './lib/tile'
+require './lib/player'
+
+class Land < Tile
+
+	def initialize(name, value_for_purchase, rent_price)
+
+		@value_for_purchase = value_for_purchase
+		@rent_price = rent_price
+		super(name)
+		@available = true
+	end
+
+	def available?
+		@available 
+	end
+
+	def sell_to_player
+		@value_for_purchase
+	end
+
+	def calculate_rent
+		@rent_price
+	end
+
+	# def to_s
+	# 	"#{@name}, #{@value_for_purchase}, #{@rent_price}"
+	# end
+	
+end
